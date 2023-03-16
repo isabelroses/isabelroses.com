d9dc1036f31df61151133a7e2083867edb659a52
isabel <isabelroseslive@gmail.com>
2023-03-16 18:56:19 +0000

    php makes life simpler

--- projects.php
+++ projects.php
@@ -1,89 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<? include 'components/head.php'; ?>
+
 <head>
-    <title>Isabel</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="color-scheme" content="dark only">
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-    <meta name="description" content="contact">
-    <meta property="og:site_name" content="isabel.contact">
-    <meta property="og:title" content="Isabel's contact">
-    <meta property="og:type" content="website">
-    <meta property="og:description" content="contact info">
-    <meta property="og:image" content="https://isabel.contact/assets/og-image.png">
-    <meta property="og:image:type" content="image/jpeg">
-    <meta property="og:image:width" content="1280">
-    <meta property="og:image:height" content="800">
-    <meta property="og:url" content="https://isabel.contact">
-    <meta property="twitter:card" content="summary_large_image">
-    <meta name="theme-color" content="#1e1e2e">
-    <link href="css/tailwind.css" rel="stylesheet" type="text/css" preload>
-    <link rel="manifest" href="manifest.json">
-    <link rel="apple-touch-icon" href="assets/icons/apple-icon-180.png">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2048-2732.jpg"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2732-2048.jpg"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1668-2388.jpg"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2388-1668.jpg"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1536-2048.jpg"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2048-1536.jpg"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1668-2224.jpg"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2224-1668.jpg"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1620-2160.jpg"
-        media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2160-1620.jpg"
-        media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1290-2796.jpg"
-        media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2796-1290.jpg"
-        media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1179-2556.jpg"
-        media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2556-1179.jpg"
-        media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1284-2778.jpg"
-        media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2778-1284.jpg"
-        media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1170-2532.jpg"
-        media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2532-1170.jpg"
-        media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1125-2436.jpg"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2436-1125.jpg"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1242-2688.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2688-1242.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-828-1792.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1792-828.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1242-2208.jpg"
-        media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-2208-1242.jpg"
-        media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-750-1334.jpg"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1334-750.jpg"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-640-1136.jpg"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-    <link rel="apple-touch-startup-image" href="assets/icons/apple-splash-1136-640.jpg"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
-    <link rel="preconnect" href="https://fonts.bunny.net">
+    <title>Projects</title>
 </head>
 
 <body>
@@ -97,71 +18,56 @@
             <h2>Projects</h2>
             <div id="projects" class="not-prose flex flex-col">
                 <div class="projects-grid">
-                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20 md:col-span-2"
-                        href="https://github.com/isabelroses/userstyles" target="_blank">
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20 md:col-span-2" href="https://github.com/isabelroses/userstyles" target="_blank">
                         <div class="flex flex-col gap-y-2">
                             <h3 class="font-bold font-display text-xl">Userstyles</h3>
                             <p class="text-fg-dimmed">A collection of all the user styles that I use</p>
                         </div>
-                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
                             <line x1="10" y1="14" x2="21" y2="3"></line>
                         </svg>
                     </a>
-                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20"
-                        href="https://github.com/catppuccin/youtube" target="_blank">
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20" href="https://github.com/catppuccin/youtube" target="_blank">
                         <div class="flex flex-col gap-y-2">
                             <h3 class="font-bold font-display text-xl">Catppuccin YouTube</h3>
                             <p class="text-fg-dimmed">A smooth pastel theme for Yotube</p>
                         </div>
-                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
                             <line x1="10" y1="14" x2="21" y2="3"></line>
                         </svg>
                     </a>
-                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20"
-                        href="https://github.com/catppuccin/freshrss" target="_blank">
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20" href="https://github.com/catppuccin/freshrss" target="_blank">
                         <div class="flex flex-col gap-y-2">
                             <h3 class="font-bold font-display text-xl">Catppuccin FreshRSS</h3>
                             <p class="text-fg-dimmed">A smooth pastel theme for FreshRSS</p>
                         </div>
-                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
                             <line x1="10" y1="14" x2="21" y2="3"></line>
                         </svg>
                     </a>
-                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20"
-                        href="https://github.com/isabelroses/isabelroses.com" target="_blank">
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20" href="https://github.com/isabelroses/isabelroses.com" target="_blank">
                         <div class="flex flex-col gap-y-2">
                             <h3 class="font-bold font-display text-xl">isabelroses.com</h3>
                             <p class="text-fg-dimmed">The source code for this website</p>
                         </div>
-                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
                             <line x1="10" y1="14" x2="21" y2="3"></line>
                         </svg>
                     </a>
-                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20"
-                        href="https://github.com/isabelroses/isabel.contact/" target="_blank">
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20" href="https://github.com/isabelroses/isabel.contact/" target="_blank">
                         <div class="flex flex-col gap-y-2">
                             <h3 class="font-bold font-display text-xl">isabel.contact</h3>
                             <p class="text-fg-dimmed">The source code to my contacts websites</p>
                         </div>
-                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
                             <line x1="10" y1="14" x2="21" y2="3"></line>
