ff6003c25d27302c55dd8e2ff4c500e22248480e
isabel <isabel@isabelroses.com>
2023-07-08 17:46:26 +0100

    feat!: remove Parsedown.php and userstyles.php

--- index.php
+++ index.php
@@ -23,10 +23,21 @@
             <h2>Projects</h2>
             <div id="projects" class="not-prose flex flex-col">
                 <div class="projects-grid">
-                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20 md:col-span-2" href="userstyles">
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20 md:col-span-2" href="https://github.com/isabelroses/dotfiles">
                         <div class="flex flex-col gap-y-2">
-                            <h3 class="font-bold font-display text-xl">Userstyles</h3>
-                            <p class="text-fg-dimmed">A collection of all the user styles that I use</p>
+                            <h3 class="font-bold font-display text-xl">Dotfiles</h3>
+                            <p class="text-fg-dimmed">All of my linux configuatrion files, prodminantly nixos</p>
+                        </div>
+                        <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                            <polyline points="15 3 21 3 21 9"></polyline>
+                            <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                    </a>
+                    <a class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20 md:col-span-2" href="https://github.com/isabelroses/bellado" target="_blank">
+                        <div class="flex flex-col gap-y-2">
+                            <h3 class="font-bold font-display text-xl">Bellado</h3>
+                            <p class="text-fg-dimmed">A cli todo tool to help you actually do your work</p>
                         </div>
                         <svg class="block w-4 h-4 mt-4 self-end" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
